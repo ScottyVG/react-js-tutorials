1c28aa2b2b52c180d9c64807252775b2ceb6b063
scottyvg <svangilder@gmail.com>
2017-01-22 18:39:58 -0700

    section 8

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { Link } from "react-router"
+import { Link } from "react-router";
+
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
-  navigate() {
-    // console.log(this.props);
-    // pushState allows back button to navigate to prior state
-    this.props.history.pushState(null, "/");
-    // replaceState state switches out right away, (back button stays on current page)
-    // this.props.history.replaceState(null, "/");
-  }
   render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+    console.log("layout");
     return (
-    <div>
-      <h1>KillerNews.net</h1>
-      {this.props.children}
-      <Link to="archives" class="btn btn-danger">archives</Link>
-      <Link to="settings"><button class="btn btn-success">settings</button></Link>
-      <button onClick={this.navigate.bind(this)}>featured</button>
-    </div>
+      <div>
+
+        <Nav location={location} />
+
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+
+              {this.props.children}
+
+            </div>
+          </div>
+          <Footer/>
+        </div>
+      </div>
+
     );
   }
 }
